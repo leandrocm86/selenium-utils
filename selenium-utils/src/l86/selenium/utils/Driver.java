6e6b24f1c25929a983375aa70b6e379fd0f85c50
leandro.medeiros@serpro.gov.br <leandro.medeiros@serpro.gov.br>
2021-12-30 16:19:31 -0300

    Projeto inicial

---
+++ selenium-utils/src/l86/selenium/utils/Driver.java
@@ -0,0 +1,165 @@
+package l86.selenium.utils;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import estruturas.Lista;
+import io.Log;
+
+public class Driver implements WebDriver {
+	
+	public final static String DRIVER_URL = "/usr/bin/chromedriver"; // apt-install chromium-chromedriver
+	
+	private WebDriver webDriver;
+	
+//	private Integer janelasAbertas = 0;
+	
+	public Driver() {
+		try {
+			this.iniciaNovoWebDriver();
+		}
+		catch(Throwable t) {
+			Log.logaErro(t);
+			throw t;
+		}
+	}
+	
+	private void iniciaNovoWebDriver() {
+//		System.setProperty("webdriver.firefox.marionette","/home/06742776609/geckodriver");
+//		instance = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", DRIVER_URL);
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
+		webDriver = new ChromeDriver(options);
+		webDriver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	}
+
+	@Override
+	public void get(String url) {
+		try {
+			webDriver.get(url);
+		} catch (org.openqa.selenium.TimeoutException | org.openqa.selenium.remote.UnreachableBrowserException e) {
+			Log.msgLn("Timeout para carregar a pagina!");
+//			try {
+//				Thread.sleep(30000);
+//			} catch (InterruptedException e1) {
+//				e1.printStackTrace();
+//			}
+	    }
+	}
+	
+	public void get(CharSequence url) {
+		this.get(url.toString());
+	}
+
+	@Override
+	public String getCurrentUrl() {
+		return webDriver.getCurrentUrl();
+	}
+
+	@Override
+	public String getTitle() {
+		return webDriver.getTitle();
+	}
+
+	@Override
+	public List<WebElement> findElements(By by) {
+		return webDriver.findElements(by);
+	}
+
+	@Override
+	public WebElement findElement(By by) {
+		return webDriver.findElement(by);
+	}
+
+	@Override
+	public String getPageSource() {
+		return webDriver.getPageSource();
+	}
+
+	@Override
+	public void close() {
+		webDriver.close();
+	}
+
+	@Override
+	public void quit() {
+		if (webDriver != null)
+			webDriver.quit();
+	}
+
+	@Override
+	public Set<String> getWindowHandles() {
+		return webDriver.getWindowHandles();
+	}
+
+	@Override
+	public String getWindowHandle() {
+		return webDriver.getWindowHandle();
+	}
+
+	@Override
+	public TargetLocator switchTo() {
+		return webDriver.switchTo();
+	}
+
+	@Override
+	public Navigation navigate() {
+		return webDriver.navigate();
+	}
+
+	@Override
+	public Options manage() {
+		return webDriver.manage();
+	}
+	
+	public void espera() {
+		espera(10000);
+	}
+	
+	public void espera(long millisecs) {
+		try {
+			Thread.sleep(millisecs);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void esperaPouco() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	public Elemento byName(String name) {
+		return new Elemento(webDriver.findElement(By.name(name)));
+	}
+	
+	public Elemento byId(String id) {
+		return new Elemento(webDriver.findElement(By.id(id)));
+	}
+	
+	public Elemento bySelector(String selector) {
+		return new Elemento(webDriver.findElement(By.cssSelector(selector)));
+	}
+	
+	public Lista<Elemento> allBySelector(String selector) {
+		return Elemento.elementos(webDriver.findElements(By.cssSelector(selector)));
+	}
+	
+	public Lista<Elemento> allByTag(String tag) {
+		return Elemento.elementos(webDriver.findElements(By.tagName(tag)));
+	}
+
+}
---
+++ selenium-utils/src/l86/selenium/utils/Elemento.java
@@ -0,0 +1,160 @@
+package l86.selenium.utils;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+
+import estruturas.Lista;
+import io.Log;
+import utils.Str;
+
+public class Elemento implements WebElement {
+	
+	private WebElement element;
+	
+	public static Lista<Elemento> elementos(List<WebElement> webElements) {
+		Lista<Elemento> retorno = new Lista<Elemento>();
+		for (WebElement element : webElements)
+			retorno.add(new Elemento(element));
+		return retorno;
+	}
+	
+	public Elemento(WebElement element) {
+		this.element = element;
+	}
+	
+	public Elemento pai() {
+		return new Elemento(element.findElement(By.xpath("./..")));
+		
+		/* Poderia ser em Javascript:
+		WebElement parent = (WebElement) ((JavascriptExecutor) driver).executeScript("return arguments[0].parentNode;", myElement)*/
+	}
+	
+	public Lista<Elemento> filhos() {
+		return elementos(element.findElements(By.xpath(".//*")));
+	}
+	
+	public Lista<Elemento> filhosBySelector(String selector) {
+		return elementos(element.findElements(By.cssSelector(selector)));
+	}
+	
+	public Elemento filhoBySelector(String selector) {
+		WebElement filho = null;
+		try {
+			filho = element.findElement(By.cssSelector(selector));
+		}
+		catch(Exception e) {
+			Log.msgLn("Elemento não encontrado para selector " + selector);
+			return null;
+		}
+		return new Elemento(filho);
+	}
+	
+	public Str texto() {
+		try {
+			return new Str(this.element.getAttribute("innerHTML"));
+		}
+		catch(Throwable t) {
+			return new Str();
+		}
+	}
+	
+	public Str atributo(String atributo) {
+		return new Str(this.element.getAttribute(atributo));
+	}
+	
+	
+	// Metodos da interface, repassados para a instancia original WebElement ----------------------------------------------------
+	
+	@Override
+	public <X> X getScreenshotAs(OutputType<X> target) throws WebDriverException {
+		return element.getScreenshotAs(target);
+	}
+
+	@Override
+	public void click() {
+		element.click();
+	}
+
+	@Override
+	public void submit() {
+		element.submit();
+	}
+
+	@Override
+	public void sendKeys(CharSequence... keysToSend) {
+		element.sendKeys(keysToSend);
+	}
+
+	@Override
+	public void clear() {
+		element.clear();
+	}
+
+	@Override
+	public String getTagName() {
+		return element.getTagName();
+	}
+
+	@Override
+	public String getAttribute(String name) {
+		return element.getAttribute(name);
+	}
+
+	@Override
+	public boolean isSelected() {
+		return element.isSelected();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return element.isEnabled();
+	}
+
+	@Override
+	public String getText() {
+		return element.getText();
+	}
+
+	@Override
+	public List<WebElement> findElements(By by) {
+		return element.findElements(by);
+	}
+
+	@Override
+	public WebElement findElement(By by) {
+		return element.findElement(by);
+	}
+
+	@Override
+	public boolean isDisplayed() {
+		return element.isDisplayed();
+	}
+
+	@Override
+	public Point getLocation() {
+		return element.getLocation();
+	}
+
+	@Override
+	public Dimension getSize() {
+		return element.getSize();
+	}
+
+	@Override
+	public Rectangle getRect() {
+		return element.getRect();
+	}
+
+	@Override
+	public String getCssValue(String propertyName) {
+		return element.getCssValue(propertyName);
+	}
+
+}
